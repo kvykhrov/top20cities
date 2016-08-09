f2e07b734e24cbb49315d140710d85cf28bffcac
kvykhrov <kvykhrov@gmail.com>
2016-08-09 14:33:23 +0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,33 @@
+import json
+import flask
+from pymongo import MongoClient
+
+
+
+app = flask.Flask(__name__) 
+
+@app.route("/") 
+def hello():
+    client = MongoClient()
+    db = client.TestDB
+    coll = db.MyCollection
+    result = coll.find().sort("pop",-1).limit(20)
+    cities = [{'misto':x['city'],'naslnnya':x['pop']} for x in result]
+    return flask.render_template("chart_bar.html", msg = cities)
+
+@app.route("/data")
+def data():
+    client = MongoClient()
+    db = client.TestDB
+    coll = db.MyCollection
+    result = coll.aggregate( [ {"$group": {"_id":"$city","naslnnia":{"$max":"$pop"}}},{"$sort":{"naslnnia":-1}},{"$limit":20}] )
+    mistagrafik = [ {'label': x['_id'] ,'value':x['naslnnia']} for x in result ]
+    return  json.dumps([ 
+    {
+      "key": "Cumulative Return",
+      "values": mistagrafik
+    }
+  ])
+
+if (__name__ == "__main__"):
+    app.run(port = 5000)
